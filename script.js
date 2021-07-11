71aebea133846e98138330b84a9003537536f5e8
Lusine1509 <andriasyanlusine@gmail.com>
2021-07-11 19:52:00 +0400

    some changes

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 console.log('hello branch 1');
-=======
+
 console.log('hello branch 2');
->>>>>>> feature/branch2
-=======
+
 console.log('hello branch 3');
->>>>>>> feature/branch3
+
